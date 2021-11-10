bec82f7517a6d71482aee43c2544a4c02440ff1a
Cody Potter <enjoipotter@gmail.com>
2021-11-09 21:10:16 -0800

    moved directory and shop data into reducer

--- src/pages/category/category.component.jsx
+++
@@ -1,9 +0,0 @@
-import './category.styles.scss'
-
-const CategoryPage = () => (
-    <div className='category'>
-        <h2>CATEGORY PAGE</h2>
-    </div>
-)
-
-export default CategoryPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import CollectionPage from "../collection/collection.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 const ShopPage = ({match}) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-        <Route path={`${match.path}/:categoryId`} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div> 
 )
 
