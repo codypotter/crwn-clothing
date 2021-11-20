743ac9a4ae945e2b39ce1da43fce9fe381c8b716
Cody Potter <enjoipotter@gmail.com>
2021-11-19 17:53:06 -0800

    implemented shop reducer, WIP

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,20 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import {connect} from "react-redux"
+import {createStructuredSelector} from "reselect"
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "../collection/collection.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import {firestore, convertCollectionSnapshotToMap} from "../../firebase/firebase.utils";
-import {updateCollections} from "../../redux/shop/shop.actions";
+import {fetchCollectionsStartAsync} from "../../redux/shop/shop.actions";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    }
-    unsubscribeFromSnapshot = null
 
     componentDidMount() {
-        const {updateCollections} = this.props
-        const collectionRef = firestore.collection('collections')
-
-        collectionRef.get().then(snapshot => {
-            const collectionsMap =  convertCollectionSnapshotToMap(snapshot)
-            updateCollections(collectionsMap)
-            this.setState({loading: false})
-        })
+        fetchCollectionsStartAsync()
     }
 
     render() {
